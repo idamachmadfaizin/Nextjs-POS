c4979a606f7339802091bedf1efe3c86c401153a
idamachmadfaizin <idamachmadfaizin@gmail.com>
2025-10-13 07:35:56 +0000

    feat: use dark mode button toggle instead of dropdown

---
+++ src/components/dark-mode-toggle.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Monitor, Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import * as React from "react";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
+
+export function DarkModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const updateTheme = React.useCallback(() => {
+    switch (theme) {
+      case "system":
+        setTheme("light");
+        break;
+      case "light":
+        setTheme("dark");
+        break;
+      case "dark":
+        setTheme("system");
+        break;
+      default:
+        setTheme("system");
+        break;
+    }
+  }, [theme]);
+
+  const iconMap: Record<string, React.ReactElement> = {
+    system: <Sun />,
+    light: <Moon />,
+    dark: <Monitor />,
+  };
+
+  const textMap: Record<string, string> = {
+    system: "light",
+    light: "dark",
+    dark: "system",
+  };
+
+  return (
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button variant="ghost" size="icon" onClick={updateTheme}>
+          {iconMap[theme ?? "system"] ?? iconMap.dark}
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Change to {textMap[theme ?? "system"] ?? textMap.dark}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
